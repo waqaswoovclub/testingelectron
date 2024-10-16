cdddee27265122220846eab2b8d130f8411d0ed2
Waqas Ahmed <waqaswoov@gmail.com>
2024-10-16 15:44:24 +0500

    fix test cases

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,9 @@
-// import '@testing-library/jest-dom';
-// import { render } from '@testing-library/react';
-// import App from '../renderer/App';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import Home from '../renderer/Home';
 
-// describe('App', () => {
-//   it('should render', () => {
-//     expect(render(<App />)).toBeTruthy();
-//   });
-// });
+describe('App', () => {
+  it('should render', () => {
+    expect(render(<Home />)).toBeTruthy();
+  });
+});
